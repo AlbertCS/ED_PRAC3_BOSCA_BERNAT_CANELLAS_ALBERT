5890501c857722dad443b4c28f04f0d9c9508847
Bernat Bosca <bernat.bosca@urv.cat>
2017-06-14 14:03:43 +0200

    Poco a poco al final se termina todo

---
+++ src/TADs/Arbre/NodeABC.java
@@ -0,0 +1,65 @@
+package TADs.Arbre;
+
+public class NodeABC<K extends Comparable<K>, V> implements Cloneable {
+	protected K k;
+	protected V v;
+	protected Arbre<K, V> fe;
+	protected Arbre<K, V> fd;
+	
+	public NodeABC(K k, V v) {
+		this.k=k;
+		this.v=v;
+		fe=null;
+		fd=null;
+	}
+
+	@Override
+	public String toString() {
+		return "NodeABC [k=" + k + ", v=" + v + ", fe=" + fe + ", fd=" + fd + "]";
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected NodeABC<K,V> clone() {
+		NodeABC<K, V> obj=null;
+		try{
+            obj=(NodeABC<K, V>)super.clone();
+        }catch(CloneNotSupportedException ex){
+            System.out.println("No es pot duplicar");
+        }
+		return obj;
+	}
+
+	public K getK() {
+		return k;
+	}
+
+	public void setK(K k) {
+		this.k = k;
+	}
+
+	public V getV() {
+		return v;
+	}
+
+	public void setV(V v) {
+		this.v = v;
+	}
+
+	public Arbre<K, V> getFe() {
+		return fe;
+	}
+
+	public void setFe(Arbre<K, V> fe) {
+		this.fe = fe;
+	}
+
+	public Arbre<K, V> getFd() {
+		return fd;
+	}
+
+	public void setFd(Arbre<K, V> fd) {
+		this.fd = fd;
+	}
+	
+}
